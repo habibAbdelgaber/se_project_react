4489f8a306befae0545da6d6742720e8dd7e3851
Habib Abdelgaber <habib.abdelgaber@gyahoo.com>
2025-07-26 16:19:08 +0300

    fix(base route): in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'se_project_react',
+  base: "/",
   server: {
     port: 3000,
     // proxy: {
